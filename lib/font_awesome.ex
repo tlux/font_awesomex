8dc20172b92112562ce128aca10fe7353becc407
Tobias Casper <tobias.casper@gmail.com>
2016-11-28 02:24:06 +0100

    removes icon stack support

--- lib/font_awesome.ex
+++ lib/font_awesome.ex
@@ -9,10 +9,17 @@ defmodule FontAwesome do
   def css_prefix, do: @css_prefix
 
   @doc """
-  Prefixes the given binary.
+  Prefixes the given binary or list.
   """
   def css_prefix(nil), do: @css_prefix
-  def css_prefix(str), do: "#{@css_prefix}-#{str}"
+
+  def css_prefix(values) when is_list(values) do
+    values
+    |> Enum.join("-")
+    |> css_prefix
+  end
+
+  def css_prefix(value), do: "#{@css_prefix}-#{value}"
 
   def icon(name, options \\ []) do
     name
--- lib/font_awesome/icon.ex
+++ lib/font_awesome/icon.ex
@@ -1,16 +1,20 @@
 defmodule FontAwesome.Icon do
   @moduledoc """
-  A struct that holds icon settings.
+  A struct to define the configuration for a specific icon.
   """
 
   use Phoenix.HTML
+
+  import FontAwesome, only: [css_prefix: 0, css_prefix: 1]
+
+  alias FontAwesome.CSS
 
   @default_fixed_width Application.get_env(:font_awesome, :fixed_width, false)
 
   @enforce_keys [:name]
   defstruct [:name, stacked: false, outline: false,
              fixed_width: @default_fixed_width, size: nil, rotate: 0, flip: nil,
-             list: false, border: false, animation: nil]
+             list: false, border: false, pull: nil, animation: nil]
 
   @doc """
   Creates new icon with the specified `name` and `options`.
@@ -42,19 +46,43 @@ defmodule FontAwesome.Icon do
     String.replace(name, "_", "-")
   end
 
-  def get_classes(%__MODULE__{} = icon) do
-    [get_icon_name(icon)] ++ get_transformations(icon)
+  defp get_classes(icon) do
+    modifiers = for {key, value} <- Map.from_struct(icon),
+                    class = get_class(key, value),
+                    !is_nil(class),
+                    do: class
+    [get_name(icon)] ++ modifiers
   end
 
-  defp get_icon_name(%{name: name, outline: true}) do
-    FontAwesome.css_prefix("#{name}-o")
+  defp get_name(%{name: name, outline: true}), do: css_prefix("#{name}-o")
+  defp get_name(%{name: name, outline: false}), do: css_prefix(name)
+
+  def get_class(_key, nil), do: nil
+
+  def get_class(:size, value) when is_integer(value) do
+    css_prefix("#{value}x")
   end
 
-  defp get_icon_name(%{name: name, outline: false}) do
-    FontAwesome.css_prefix(name)
+  def get_class(:size, value), do: css_prefix(value)
+
+  def get_class(:fixed_width, true), do: css_prefix("fw")
+
+  def get_class(:stacked, true), do: css_prefix("stack")
+
+  def get_class(:rotate, 0), do: nil
+  def get_class(:rotate, value) when is_integer(value) do
+    css_prefix(["rotate", value])
   end
 
-  defp get_transformations(icon) do
-    [] # TODO
-  end
+  def get_class(:flip, direction), do: css_prefix(["flip", direction])
+
+  def get_class(:list, true), do: css_prefix("li")
+
+  def get_class(:border, true), do: css_prefix("border")
+
+  def get_class(:pull, side), do: css_prefix(["pull", side])
+
+  def get_class(:animation, style), do: css_prefix(style)
+
+  def get_class(_key, _value), do: nil
 end
