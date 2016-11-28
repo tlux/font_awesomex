40044cb81d0037a123cee41a41d1e4ab2a637101
Tobias Casper <tobias.casper@gmail.com>
2016-11-29 00:47:13 +0100

    renames app to font_awesomex

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule FontAwesome.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :font_awesome,
+    [app: :font_awesomex,
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
