390382d75869760f44faa5a1585b1dac1f7db3bd
Tobias Casper <tobias.casper@gmail.com>
2016-11-29 02:27:59 +0100

    adds ex_doc

--- mix.exs
+++ mix.exs
@@ -3,13 +3,27 @@ defmodule FontAwesome.Mixfile do
 
   use Mix.Project
 
+  @description """
+  A library that provides a helper to easily integrate Font Awesome into your
+  Phoenix application.
+  """
+
   def project do
     [app: :font_awesomex,
      version: "4.0.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps,
+     description: @description,
+     package: package,
+
+     # Docs
+     name: "FontAwesome",
+     source_url: "https://github.com/tlux/font_awesomex",
+     docs: [main: "FontAwesome",
+            extras: ["README.md"]]
+   ]
   end
 
   # Configuration for the OTP application
@@ -30,6 +44,19 @@ defmodule FontAwesome.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:phoenix_html, "~> 2.6"},
-     {:credo, "~> 0.4", only: :dev}]
+     {:credo, "~> 0.4", only: :dev},
+     {:ex_doc, "~> 0.14", only: :dev}]
+  end
+
+  defp package do
+    [
+      name: :font_awesomex,
+      files: [], # TODO
+      maintainers: ["Tobias Casper"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/tlux/font_awesomex"
+      }
+    ]
   end
 end
